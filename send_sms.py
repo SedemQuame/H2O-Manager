b288eaf7e0022cbb7d2617da5913479aab5b5f81
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-07-21 22:03:05 +0000

    refactored sms application.

--- send_sms.py
+++ send_sms.py
@@ -4,16 +4,20 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC214c46532167b2de9f4bfb537a3191cf'
-auth_token = 'ad480870f128c2e3bbddf02f3a914ef5'
-client = Client(account_sid, auth_token)
+ACCOUNT_SID = 'AC214c46532167b2de9f4bfb537a3191cf'
+AUTH_TOKEN  = 'ad480870f128c2e3bbddf02f3a914ef5'
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
-message = client.messages \
-                .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+19166190482',
-                     to='+233546744163'
-                 )
+def send_sms():
+	""" Simple function that sends sms using the twilio api."""
+	message = client.messages \
+        	        .create(
+                	     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+	                     from_='+19166190482',
+        	             to='+233546744163'
+                	 )
+	print(message.sid)
+	return(message.sid)
 
-print(message.sid)
+send_sms()
 
