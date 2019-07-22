54d77d1e131fa8d808477c7187f40414b0b6606f
SedemQuame <sedem.amekpewu.3@gmail.com>
2019-07-22 22:57:30 +0000

    made project lean, by removing irrelevant files from the directory

--- read_template.py
+++
@@ -1,8 +0,0 @@
-""" A simple function to read the message template. """
-
-from string import Template
-
-def read_template(filename):
-	with open(filename, 'r', encoding='utf-8') as template_file:
-		template_file_content = template_file.read()
-	return Template(template_file_content)
--- sendemail.py
+++
@@ -1,9 +0,0 @@
-# Try running this locally.
-def send_simple_message():
-    return requests.post(
-        "https://api.mailgun.net/v3/samples.mailgun.org/messages",
-        auth=("api", "key-3ax6xnjp29jd6fds4gc373sgvjxteol0"),
-        data={"from": "sedem.amekpewu.3@gmail.com",
-              "to": ["sedem.amekpewu.3@gmail.com"],
-              "subject": "Hello",
-              "text": "Testing some Mailgun awesomeness!"})
